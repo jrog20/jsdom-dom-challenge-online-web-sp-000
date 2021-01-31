697ca113c8698778525be148d12789ba252fcde7
jrog20 <jaimerogstad@gmail.com>
2021-01-31 10:06:27 -0500

    commit tree 2724e2416e7c522c557dbd427fd8c1fdd1f54ab3

---
+++ js/challenge.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  // timer increment every second
+
+  // manually increment and decrement the counter
+  // using the plus and minus buttons
+
+  // I can 'like' an individual number of the counter. 
+  // I see count of the number of 'likes' associated with that number
+
+  // pause the counter:
+    // 1. pause the counter
+    // 2. disable all buttons except the pause button
+    // 3. the pause button should then show the text "resume."
+
+  // When 'resume' is clicked, it should restart the counter and re-enable the buttons.
+
+  // I can leave comments on my gameplay,
+
+});
+
